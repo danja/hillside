d5d76e59bf7671422cf7c7bd1df260e8303bf065
Danny Ayers <danny.ayers@gmail.com>
2025-08-26 17:36:42 +0200

    path

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig({
-  base: '/hillside/dist/',
-  root: './src',
-  publicDir: '../public',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom'
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    base: command === 'build' ? '/hillside/' : '/',
+    root: './src',
+    publicDir: '../public',
+    build: {
+      outDir: '../dist',
+      emptyOutDir: true
+    },
+    server: {
+      port: 3000
+    },
+    preview: {
+      port: 3000
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom'
+    }
   }
 })
